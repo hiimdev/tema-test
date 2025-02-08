a2c081a81705268cebae5f7772a32099f1379caf
Huy Vo <huyvo@var-meta.com>
2025-02-09 05:08:29 +0700

    style: header mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,61 +1,133 @@
-import React from 'react'
-import './header.css'
-import SvgIcon from '../SvgIcon'
-import { images } from '../../constants'
-import { useState } from 'react'
+import React, { useState } from "react";
 import clsx from "clsx";
+import "./header.css";
+import SvgIcon from "../SvgIcon";
+import { images } from "../../constants";
 
 const Header = () => {
+  const [lang, setLang] = useState("EN");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      {/* Mobile Header */}
+      <header className="header-container-mobile">
+        <div className="header-left-mobile">
+          <SvgIcon name="logo" size={32} />
+          <p className="brand">macode.</p>
+        </div>
+
+        <LanguageDropdown value={lang} onChange={setLang} />
+
+        <div style={{ marginLeft: "30px" }} onClick={() => setIsMobileMenuOpen(true)}>
+          <SvgIcon name="menuMobile" size={19} />
+        </div>
+      </header>
+
+      {/* Desktop Header */}
+      <header className="header-container">
+        <div className="header-left">
+          <SvgIcon name="logo" size={32} />
+          <nav className="menu-wrap">
+            <div className="menu">
+              {["Tutorials", "Case studies", "Resources"].map((item) => (
+                <a key={item} className="menu-item" href="#">
+                  {item}
+                </a>
+              ))}
+            </div>
+            <LanguageDropdown value={lang} onChange={setLang} />
+          </nav>
+        </div>
+      </header>
+
+      {/* Mobile Menu Overlay */}
+      <MobileMenu isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} />
+    </>
+  );
+};
+
+export default Header;
+
+const LanguageDropdown = ({ value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState('EN');
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
-    <header className='header-container'>
-      <div className='header-left'>
-        <SvgIcon name="logo" size={32} />
+    <div className="location-wrap" onClick={toggleDropdown}>
+      <div className="location-right">
+        <img
+          src={value === "EN" ? images.us : images.vn}
+          alt="language"
+          className="img-location"
+          width={21}
+          height={21}
+        />
+        <p className="text-location">{value}</p>
+      </div>
 
-        <div className='menu-wrap'>
-          <div className='menu'>
-            <a className='menu-item' href='#'>Tutorials</a>
+      <span className="location-icon">
+        <SvgIcon name="chevronUp" size={20} />
+      </span>
 
-            <p style={{ lineHeight: 0, marginBottom: '10px' }}>.</p>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {["EN", "VN"].map((lang) => (
+            <li key={lang} className={clsx(value === lang && "active")} onClick={() => onChange(lang)}>
+              <img
+                src={lang === "EN" ? images.us : images.vn}
+                alt={lang}
+                className="img-location"
+                width={21}
+                height={21}
+              />
+              <p>{lang === "EN" ? "ENGLISH" : "VIỆT NAM"}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-            <a className='menu-item' href='#'>Case studies</a>
+const MobileMenu = ({ isOpen, onClose }) => {
+  return (
+    <div className={clsx("menu-mobile", isOpen && "open-menu-mobile")}>
+      <div className="menu-mobile-wrap">
+        <div className="menu-mobile-header" style={{ justifyContent: "flex-end", display: "flex" }}>
+          <span className="menu-mobile-close" onClick={onClose}>
+            <SvgIcon name="close" size={24} />
+          </span>
+        </div>
 
-            <p style={{ lineHeight: 0, marginBottom: '10px' }}>.</p>
+        <ul className="menu-mobile-list">
+          {[
+            { name: "Tutorials", icon: "setting" },
+            { name: "Case studies", icon: "case" },
+            { name: "Resources", icon: "resourse" },
+          ].map((item) => (
+            <li key={item.name} className="menu-mobile-item">
+              <p>
+                <SvgIcon name={item.icon} size={30} />
+                {item.name}
+              </p>
+              <SvgIcon name="chevronUp" className="menu-mobile-icon-arrow" size={24} />
+            </li>
+          ))}
+        </ul>
 
-            <a className='menu-item' href='#'>Resources</a>
-          </div>
-
-
-          <div className='location-wrap' onClick={toggleDropdown}>
-            <div className='location-right'>
-              <img src={images.us} alt="us" className='img-location' width={21} height={21} />
-              <p className='text-location'>{value === 'EN' ? 'EN' : 'VN'}</p>
-            </div>
-
-            <span className='location-icon'>
-              <SvgIcon name="chevronUp" size={20} />
-            </span>
-            {isOpen && (
-              <ul className="dropdown-menu">
-                <li className={clsx(value === 'EN' && "active")} onClick={() => setValue('EN')}>
-                  <img src={images.us} alt="us" className='img-location' width={21} height={21} />
-                  <p>ENGLISH</p>
-                </li>
-                <li className={clsx(value === 'VN' && "active")} onClick={() => setValue('VN')}>
-                  <img src={images.vn} alt="us" className='img-location' width={21} height={21} />
-                  <p>VIỆT NAM</p>
-                </li>
-              </ul>
-            )}
-          </div>
+        <div className="menu-mobile-social-wrap">
+          <ul className="menu-mobile-social">
+            {["facebook", "ins", "linkedin", "twitter", "gmail"].map((social) => (
+              <li key={social}>
+                <SvgIcon name={social} size={32} />
+              </li>
+            ))}
+          </ul>
+          <p className="menu-mobile-copyright">© 2021 Copyright. Macode.io</p>
         </div>
       </div>
-    </header>
-  )
-}
-
-export default Header
+    </div>
+  );
+};
